fb742b89c7a550b201ff4a7f4e51ad44ad726115
Wiltonadm <49535876+Wiltonadm@users.noreply.github.com>
2019-07-10 14:32:36 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,103 +1,117 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangerListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
 	private Department entity;
-	
+
 	private DepartmentService service;
-	
+
+	private List<DataChangerListener> dataChangerListeners = new ArrayList<>();
+
 	@FXML
 	private TextField txId;
-	
+
 	@FXML
 	private TextField txName;
-	
+
 	@FXML
 	private Label labelErroName;
-	
+
 	@FXML
 	private Button btSave;
-	
+
 	@FXML
 	private Button btCancel;
-	
+
 	public void setDepartment(Department entity) {
 		this.entity = entity;
 	}
-	
+
 	public void setDepartmenteService(DepartmentService service) {
 		this.service = service;
 	}
-	
+
+	public void subscribeDataChangerListener(DataChangerListener listener) {
+		dataChangerListeners.add(listener);
+	}
+
 	@FXML
 	public void onBtCancelAction(ActionEvent event) {
 		Utils.currentStage(event).close();
 	}
-	
+
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
-		
-		if(entity == null ) {
+
+		if (entity == null) {
 			throw new IllegalStateException("Entity was null");
-		}		
-		if(service == null ) {
+		}
+		if (service == null) {
 			throw new IllegalStateException("Service was null");
 		}
 		try {
 			entity = getFormDate();
 			service.saveOrUpdate(entity);
+			notifyDataChangerListeners();
 			Utils.currentStage(event).close();
-		}
-		catch(DbException e ) {
-			Alerts.showAlert("Error Saving object", null ,  e.getMessage(), AlertType.ERROR);
+		} catch (DbException e) {
+			Alerts.showAlert("Error Saving object", null, e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
+
+	private void notifyDataChangerListeners() {
+		for (DataChangerListener listener : dataChangerListeners) {
+			listener.onDataChanged();
+		}
+	}
+
 	private Department getFormDate() {
-		
+
 		Department obj = new Department();
-		
+
 		obj.setId(Utils.tryParseToInt(txId.getText()));
 		obj.setName(txName.getText());
-		
+
 		return obj;
-		
+
 	}
 
 	@Override
-	public void initialize(URL url, ResourceBundle rb) {	
+	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
 	}
-	
+
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txId);
 		Constraints.setTextFieldMaxLength(txName, 30);
 	}
 
-	
 	public void updateFormData() {
-		if(entity == null ) {
+		if (entity == null) {
 			throw new IllegalStateException("Entity was null");
 		}
 		txId.setText(String.valueOf(entity.getId()));
 		txName.setText(entity.getName());
 	}
-	
+
 }
--- src/gui/DepartmentListControlle.java
+++ src/gui/DepartmentListControlle.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.listeners.DataChangerListener;
 import gui.util.Alerts;
 import gui.util.Utils;
 import javafx.collections.FXCollections;
@@ -26,7 +27,7 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListControlle implements Initializable {
+public class DepartmentListControlle implements Initializable, DataChangerListener {
 
 	
 	private DepartmentService service;
@@ -95,6 +96,8 @@ public class DepartmentListControlle implements Initializable {
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
 			controller.setDepartmenteService(new DepartmentService());
+			controller.subscribeDataChangerListener(this);
+			
 			controller.updateFormData();
 			
 			Stage dialogState = new Stage();
@@ -111,6 +114,9 @@ public class DepartmentListControlle implements Initializable {
 			Alerts.showAlert("IO Excepetio", "Error loanding view", e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
 
+	@Override
+	public void onDataChanged() {
+		updateTableView();		
+	}
 }
